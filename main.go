e04376b994b538afd3c943358816a9de59d2560f
c45y <git@c45y.com>
2021-01-21 17:56:24 +1000

    structs are the way to go it seems

--- main.go
+++ main.go
@@ -1,15 +1,49 @@
 package main
 
 import (
-	"net/http"
-	"encoding/json"
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
-	"strings"
 	"log"
+	"net/http"
+	"strings"
 )
 
 const SearchURI = "https://www.data.qld.gov.au/api/3/action/package_search?q=organization:griffith-university"
+
+type SearchResults struct {
+	Help   string       `json:"help"`
+	Success bool `json:"success"`
+	Result SearchResult `json:"result"`
+}
+
+type SearchResult struct {
+	Count   int      `json:"count"`
+	Results []Result `json:"results`
+}
+
+type Result struct {
+	Author        string     `json:"author"`
+	AuthorEmail   string     `json:"author_email"`
+	CreatorUserId string     `json:"creator_user_id"`
+	Id            string     `json:"id"`
+	Maintainer    string     `json:"maintainer"`
+	Name          string     `json:"name"`
+	Resources     []Resource `json:"resources"`
+}
+
+type Resource struct {
+	Created      string `json:"created"`
+	Description  string `json:"description"`
+	Format       string `json:"format"`
+	Id           string `json:"id"`
+	LastModified string `json:"last_modified"`
+	Mimetype     string `json:"mimetype"`
+	Name         string `json:"name"`
+	OriginalUrl  string `json:"original_url"`
+	RevisionId   string `json:"revision_id"`
+	Url          string `json:"url"`
+}
 
 func in(haystack []string, needle string) bool {
 	for _, item := range haystack {
@@ -29,7 +63,7 @@ func dieOnError(e error) {
 func loadResource(uri string) {
 	colors := []string{"\x1b[37m", "\x1b[90m"}
 
-	fmt.Printf("%#v\n\n", uri)
+	fmt.Printf("Loading: %s\n\n", uri)
 
 	res, err := http.Get(uri)
 	dieOnError(err)
@@ -48,46 +82,22 @@ func loadResource(uri string) {
 				continue
 			}
 		}
-		fmt.Printf("%s%s\x1b[0m", colors[i % 2], strings.Join(record, "\t"))
+		fmt.Printf("%s%s\x1b[0m", colors[i%2], strings.Join(record, "\t"))
 	}
-}
-
-// There are neater ways to handle this, rewrite for nested structs
-func parseResources(raw map[string]interface{}) map[string][]string {
-	results := map[string][]string{}
-
-	if data_result, ok := raw["result"].(map[string]interface{}); ok {
-		if data_results, ok := data_result["results"]; ok {
-			for _, rinter := range data_results.([]interface{}) {
-				result := rinter.(map[string]interface{})
-				if resources, ok := result["resources"]; ok {
-					for _, rres := range resources.([]interface{}) {
-						resource := rres.(map[string]interface{})
-						if url, ok := resource["url"]; ok {
-							results[result["name"].(string)] = append(results[result["name"].(string)], url.(string))
-						}
-					}
-				}
-			}
-		}
-	}
-	return results
 }
 
 func main() {
 	res, err := http.Get(SearchURI)
 	dieOnError(err)
 
-	var data_raw map[string]interface{}
+	var data_raw SearchResults
 	err = json.NewDecoder(res.Body).Decode(&data_raw)
 	dieOnError(err)
 
-	resources := parseResources(data_raw)
-
-	for name, resources := range resources {
-		if strings.Contains(name, "contract") {
-			for _, uri := range resources {
-				loadResource(uri)
+	for _, result := range data_raw.Result.Results {
+		if strings.Contains(result.Name, "contract") {
+			for _, res := range result.Resources {
+				loadResource(res.Url)
 			}
 		}
 	}
